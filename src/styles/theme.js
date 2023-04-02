14e15746529746c9a9a6bb279eecc63dcf19e536
Lucas Wendell <lucaswhendellf@gmail.com>
2023-04-02 15:19:31 -0300

    style: creating themes

---
+++ src/styles/theme.js
@@ -0,0 +1,33 @@
+export const dark = {
+	fontWeights: {
+		regular: "400",
+		bold: "700",
+	},
+	colors: {
+		activeColor: "hsl(220, 98%, 61%)",
+		bgColor: "hsl(0, 0%, 98%)",
+		navColor: "hsl(0, 0%, 98%)",
+		circleColor: "hsl(236, 33%, 92%)",
+		primaryTextColor: "hsl(235, 19%, 35%)",
+		secondaryTextColor: "hsl(233, 11%, 84%)",
+		tertiaryTextColor: "hsl(236, 9%, 61%)",
+		taskBgColor: "hsl(0, 0%, 98%)",
+	},
+};
+
+export const light = {
+	fontWeights: {
+		regular: "400",
+		bold: "700",
+	},
+	colors: {
+		activeColor: "hsl(220, 98%, 61%)",
+		bgColor: "hsl(235, 21%, 11%)",
+		navColor: "hsl(0, 0%, 98%)",
+		circleColor: "hsl(234, 11%, 52%)",
+		primaryTextColor: "hsl(236, 33%, 92%)",
+		secondaryTextColor: "hsl(237, 14%, 26%)",
+		tertiaryTextColor: "hsl(234, 11%, 52%)",
+		taskBgColor: "hsl(235, 24%, 19%)",
+	},
+};
