e0af246179e73e76274f886919274c087fe67632
Lucas Wendell <lucaswhendellf@gmail.com>
2023-04-03 15:24:12 -0300

    feat: creating route login

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import { Home } from "./page/App";
 import { GlobalStyles } from "./styles/global-styles";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./page/Login";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Home />,
 	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
 ]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<RouterProvider router={router} />
