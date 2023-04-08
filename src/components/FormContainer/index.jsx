c0dd2b113ad76ea6232b9134431b9856f9dd006a
Lucas Wendell <lucaswhendellf@gmail.com>
2023-04-08 10:48:07 -0300

    styles: creating style for the link

--- src/components/FormContainer/index.jsx
+++ src/components/FormContainer/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Paragraph } from "./styles";
 import { Form } from "./styles";
 import { InputContainer } from "./styles";
 import { FormInput } from "../FormInput";
 import { AiOutlineUser } from "react-icons/ai";
 import { RiLockPasswordLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 export const FormContainer = () => {
 	return (
 		<Container>
@@ -30,9 +31,9 @@ export const FormContainer = () => {
 					/>
 				</InputContainer>
 			</Form>
-			<p>
-				Don&apos;t have account?<span>Sign Up</span>
-			</p>
+			<Paragraph>
+				Don&apos;t have account?<Link>Sign Up</Link>
+			</Paragraph>
 		</Container>
 	);
 };
--- src/components/FormContainer/styles.js
+++ src/components/FormContainer/styles.js
@@ -1,29 +1,63 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Container = styled.div`
+	gap: 2.5rem;
 	width: 100%;
 	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
 	padding-top: 3rem;
-	gap: 2.5rem;
+	align-items: center;
+	flex-direction: column;
+	justify-content: center;
 `;
 
 export const Form = styled.div`
+	gap: 2.5rem;
 	width: 100%;
 	display: flex;
 	align-items: center;
-	justify-content: center;
 	flex-direction: column;
-	gap: 2.5rem;
+	justify-content: center;
 `;
 
 export const InputContainer = styled.div`
 	width: 100%;
+	gap: 1.5rem;
 	display: flex;
 	align-items: center;
-	justify-content: center;
 	flex-direction: column;
-	gap: 1.5rem;
+	justify-content: center;
+`;
+
+export const Paragraph = styled.p`
+	${({ theme }) => css`
+		gap: 0.2rem;
+		display: flex;
+		font-size: 1.2rem;
+		align-items: center;
+		justify-content: center;
+		letter-spacing: 0.09rem;
+		font-weight: ${theme.fontWeights.bold};
+		color: ${theme.colors.primaryTextColor};
+
+		& > a {
+			position: relative;
+			text-decoration: none;
+			color: ${theme.colors.activeColor};
+		}
+
+		& > a::after{
+			left: 0;
+			width: 0;
+			content: '';
+			height: .2rem;
+			bottom: -0.2rem;
+			position: absolute;
+      transition: all 300ms ease-in-out;
+		}
+
+		& > a:hover::after {
+			width: 100%;
+			height: .2rem;
+			background-color: ${theme.colors.activeColor};
+	`}
 `;
