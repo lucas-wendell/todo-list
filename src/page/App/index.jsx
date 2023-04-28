bf9b1d5c5415159308501f552307926a959fec13
Lucas Wendell <lucaswhendellf@gmail.com>
2023-04-28 14:44:10 -0300

    feat: turn home in a private route

--- src/page/App/index.jsx
+++ src/page/App/index.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+	const navigate = useNavigate();
+	const uid = localStorage.getItem("uid");
+
+	useEffect(() => {
+		if (uid === "" || !uid) {
+			navigate("/login");
+			return;
+		}
+	});
+
 	return <h1>Todo list</h1>;
 };
