c75d9258b71a7c4b0780b7a4fd7768a11301693f
Lucas Wendell <lucaswhendellf@gmail.com>
2023-05-03 16:05:41 -0300

    feat: add circle component

---
+++ src/components/Circle/styles.js
@@ -0,0 +1,10 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+	${({ theme }) => css`
+		width: 1.5rem;
+		height: 1.5rem;
+		border: 0.1rem solid ${theme.colors.circleColor};
+		border-radius: 50%;
+	`}
+`;
