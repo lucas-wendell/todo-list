d96bf64d085488a1876945c86fc202c0869381fb
Lucas Wendell <lucaswhendellf@gmail.com>
2023-05-06 11:01:50 -0300

    feat: create task actions

--- src/taskContext/index.jsx
+++ src/taskContext/index.jsx
@@ -3,7 +3,23 @@ import React, { createContext, useReducer } from "react";
 import { reducer } from "./reducer";
 import actions from "./actions";
 
-export const TasksContext = createContext({ teste: "teste" });
+export const TasksContext = createContext([
+	{
+		id: 1,
+		title: "teste",
+		isCompleted: false,
+	},
+	{
+		id: 2,
+		title: "teste 2",
+		isCompleted: true,
+	},
+	{
+		id: 3,
+		title: "teste 3",
+		isCompleted: false,
+	},
+]);
 
 export const TasksProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, TasksContext);
@@ -11,6 +27,15 @@ export const TasksProvider = ({ children }) => {
 		state,
 		teste: () => {
 			dispatch({ type: actions.TESTE });
+		},
+		deleteTask: (id) => {
+			dispatch({ type: actions.DELETE_TASK, payload: id });
+		},
+		addTask: (id) => {
+			dispatch({ type: actions.ADD_TASK, payload: id });
+		},
+		toggleTaskState: (id) => {
+			dispatch({ type: actions.TOGGLE_COMPLETED_STATE_TASK, payload: id });
 		},
 	};
 
--- src/taskContext/reducer.js
+++ src/taskContext/reducer.js
@@ -6,6 +6,18 @@ export const reducer = (state, action) => {
 			console.log("testando...");
 			return { ...state };
 		}
+		case actions.ADD_TASK: {
+			console.log("ADD...");
+			return { ...state };
+		}
+		case actions.DELETE_TASK: {
+			console.log("DELETE...");
+			return { ...state };
+		}
+		case actions.TOGGLE_COMPLETED_STATE_TASK: {
+			console.log("TOGGLE...");
+			return { ...state };
+		}
 		default:
 			return { ...state };
 	}
