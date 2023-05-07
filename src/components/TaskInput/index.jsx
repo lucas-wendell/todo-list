eb563a37f4dabf544030db02333e6130752ea338
Lucas Wendell <lucaswhendellf@gmail.com>
2023-05-07 14:30:24 -0300

    feat: handle on enter key is pressed in task input

--- src/components/TaskInput/index.jsx
+++ src/components/TaskInput/index.jsx
@@ -4,10 +4,21 @@ import { Label, Input } from "./styles";
 import { Circle } from "../Circle";
 
 export const TaskInput = () => {
+	const handleOnKeyDown = (event) => {
+		const keyName = event.key.toLowerCase();
+		if (keyName === "enter" && event.target.value) {
+			console.log("opaaaaa");
+			console.log(event.target.value);
+		}
+	};
+
 	return (
 		<Label>
 			<Circle />
-			<Input placeholder="Type your task title here!" />
+			<Input
+				placeholder="Type your task title here!"
+				onKeyDown={handleOnKeyDown}
+			/>
 		</Label>
 	);
 };
