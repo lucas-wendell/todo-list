ccdf77bb03fa63c09a5d3aa21051449701b71dc7
Lucas Wendell <lucaswhendellf@gmail.com>
2023-05-10 13:51:46 -0300

    style: add box shadow on tasks container

--- src/components/TasksContainer/styles.js
+++ src/components/TasksContainer/styles.js
@@ -4,8 +4,10 @@ export const Container = styled.div`
 	${({ theme }) => css`
 		background-color: ${theme.colors.taskBgColor};
 		border-radius: 0.2rem;
-		/* min-height: 20rem; */
 		display: flex;
 		flex-direction: column;
+		-webkit-box-shadow: 0px 0px 49px -9px rgba(0, 0, 0, 0.56);
+		-moz-box-shadow: 0px 0px 49px -9px rgba(0, 0, 0, 0.56);
+		box-shadow: 0px 0px 49px -9px rgba(0, 0, 0, 0.56);
 	`}
 `;
